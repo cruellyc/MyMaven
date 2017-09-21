cb058c7dc3f049525eece19d6e57f79850f1079d
cruel <2311499647@qq.com>
2017-09-21 11:36:49 +0800

    打包测试

---
+++ debug/mkapk.bat
@@ -0,0 +1,22 @@
+﻿@echo off
+
+set pkgName=LycObject
+set appName=测试打包
+
+rd /s /q %pkgName%
+
+call cordova create %pkgName% com.liyc.%pkgName% "%appName%"
+copy /y config\config.xml %pkgName%\config.xml
+xcopy /s config\res %pkgName%\res\
+
+cd %pkgName%
+rd /s /q www
+md www
+xcopy /s ..\..\src\main\webapp\html\phone www\
+
+call cordova platform add android
+call cordova build android
+cd ..
+copy %pkgName%\platforms\android\ant-build\CordovaApp-debug.apk output\%pkgName%.apk
+
+:EOF
